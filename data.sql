1ffaba736594b018196effc84e8581c4da936565
Thiri <thirihtet246@gmail.com>
2023-03-27 15:42:24 +0630

    Insert datas

--- data.sql
+++ data.sql
@@ -1,5 +1,7 @@
 /* Populate database with sample data. */
 
-INSERT INTO animals (name) VALUES ('Luna');
-INSERT INTO animals (name) VALUES ('Daisy');
-INSERT INTO animals (name) VALUES ('Charlie');
+INSERT INTO animals (name, date_of_birth, escape_attempts, neutered, weight_kg) 
+VALUES ('Agumon',DATE '2020-03-02', 0, true,  10.23),
+('Gabumon',DATE '2018-11-15', 2, true,  8),
+('Pikachu',DATE '2021-01-07', 1, false,  15.04),
+('Devimon',DATE '2017-05-12', 5, true,  11);
