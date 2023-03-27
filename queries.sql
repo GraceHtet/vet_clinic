4b72dc01ada57d6c65a7b633a58b625a76e0e47b
Thiri <thirihtet246@gmail.com>
2023-03-27 23:04:39 +0630

    Insert data

--- queries.sql
+++ queries.sql
@@ -1,5 +1,6 @@
 /*Queries that provide answers to the questions from all projects.*/
 
+--create table
 SELECT * FROM animals WHERE name LIKE '%mon';
 SELECT name FROM animals WHERE date_of_birth BETWEEN '2016-01-01' AND '2019-01-01';
 SELECT name FROM animals WHERE neutered = true AND escape_attempts< 3;
